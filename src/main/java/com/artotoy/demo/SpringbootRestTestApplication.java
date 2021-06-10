1371cfd347e9cf0de28ad46449226bb1e176e192
Arthur Olano <artotoy@gmail.com>
2021-06-10 03:34:13 -0400

    initial commit

---
+++ src/main/java/com/artotoy/demo/SpringbootRestTestApplication.java
@@ -0,0 +1,13 @@
+package com.artotoy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestTestApplication.class, args);
+	}
+
+}
